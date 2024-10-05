81471b9cc1fd79c698be0dfdb5fa74c0d61f0e52
GodsplanELot <elotgodsplan2@gmail.com>
2024-10-05 14:41:11 +0100

    updated logic syntax formatting

--- main.js
+++ main.js
@@ -3,14 +3,17 @@
 //sytanx
 //condition ? ifTrue : ifFalse
 
-
-let playerOne = "rock"
+let playerOne = "rock";
 let computer = "rock";
 
-let result = playerOne === computer ? "Tie game!" :
-playerOne ===  "rock" && computer === "paper"? "computer wins!"
-: playerOne === "paper" && computer === "scissors" ?
-"computer wins!"
-:playerOne === "scissors" && computer  ? "computer wins"
-: "playerOne wins!";
-console.log(result)
+let result =
+  playerOne === computer
+    ? "Tie game!"
+    : playerOne === "rock" && computer === "paper"
+    ? "computer wins!"
+    : playerOne === "paper" && computer === "scissors"
+    ? "computer wins!"
+    : playerOne === "scissors" && computer
+    ? "computer wins"
+    : "playerOne wins!";
+console.log(result);
