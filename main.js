bac9ba057c7dcc329c87bdb3227739827e17caf2
GodsplanELot <elotgodsplan2@gmail.com>
2024-10-05 06:52:06 +0100

    created the html boiler palte and js file, this is my first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+//tenary operators
+
+//sytanx
+//condition ?
+
+
+
+let fish = Math.floor(Math.random())
+
+if (fish === 1){
+    //run this code
+    console.log("yes come and buy")
+}else{
+    //run this instead 
+    console.log("wait till mr uche bbq day")
+}
