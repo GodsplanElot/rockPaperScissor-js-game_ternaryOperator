5a62bc3987c7bc1268ef1054c05d8ca3e4086dae
GodsplanELot <elotgodsplan2@gmail.com>
2024-10-05 07:09:24 +0100

    updated the js logic,

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 //tenary operators
 
 //sytanx
-//condition ?
+//condition ? ifTrue : ifFalse
 
 
+let playerOne = "rock"
+let computer = "rock";
 
-let fish = Math.floor(Math.random())
-
-if (fish === 1){
-    //run this code
-    console.log("yes come and buy")
-}else{
-    //run this instead 
-    console.log("wait till mr uche bbq day")
-}
+let result = playerOne === computer ? "Tie game!" :
+playerOne ===  "rock" && computer === "paper"? "computer wins!"
+: playerOne === "paper" && computer === "scissors" ?
+"computer wins!"
+:playerOne === "scissors" && computer  ? "computer wins"
+: "playerOne wins!";
+console.log(result)
